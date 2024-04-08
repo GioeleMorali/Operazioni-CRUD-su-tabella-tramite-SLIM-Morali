1b78cfe748f8a31fc899f29f8cd25e84b7fa4781
lai2-24 <lai2-24@itismeucci.com>
2024-04-08 11:11:16 +0200

    Final final commit

--- php/controllers/AlunniController.php
+++ php/controllers/AlunniController.php
@@ -12,4 +12,52 @@ class AlunniController
     $response->getBody()->write(json_encode($results));
     return $response->withHeader("Content-type", "application/json")->withStatus(200);
   }
+  public function getAlunno(Request $request, Response $response, $args){
+    $id = $args['id'];
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $result = $mysqli_connection->query("SELECT * FROM alunni WHERE id = $id");
+    $results = $result->fetch_all(MYSQLI_ASSOC);
+
+    $response->getBody()->write(json_encode($results));
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+  public function postAlunni(Request $request, Response $response, $args){
+    $dati = json_decode($request->getBody()->getContents(), true);
+    $nome = $dati["nome"];
+    $cognome = $dati["cognome"];
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $result = $mysqli_connection->query("INSERT INTO alunni (nome, cognome) VALUES ('$nome', '$cognome')");
+    $result2 = $mysqli_connection->query("SELECT * FROM alunni");
+    $results = $result2->fetch_all(MYSQLI_ASSOC);
+
+
+    $response->getBody()->write(json_encode($results));
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+  public function putAlunno(Request $request, Response $response, $args){
+    $dati = json_decode($request->getBody()->getContents(), true);
+    $id = $args['id'];
+    $nome = $dati["nome"];
+    $cognome = $dati["cognome"];
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $result = $mysqli_connection->query("UPDATE alunni SET nome='$nome', cognome='$cognome' WHERE id= '$id'");
+    $result2 = $mysqli_connection->query("SELECT * FROM alunni");
+    $results = $result2->fetch_all(MYSQLI_ASSOC);
+
+
+    $response->getBody()->write(json_encode($results));
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+  public function deleteAlunno(Request $request, Response $response, $args){
+    $dati = json_decode($request->getBody()->getContents(), true);
+    $id = $args['id'];
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $result = $mysqli_connection->query("DELETE FROM alunni WHERE id=$id");
+    $result2 = $mysqli_connection->query("SELECT * FROM alunni");
+    $results = $result2->fetch_all(MYSQLI_ASSOC);
+
+
+    $response->getBody()->write(json_encode($results));
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
 }
--- php/index.php
+++ php/index.php
@@ -7,5 +7,8 @@ require __DIR__ . '/controllers/AlunniController.php';
 $app = AppFactory::create();
 
 $app->get('/alunni', "AlunniController:index");
-
+$app->get('/alunni/{id}', "AlunniController:getAlunno");
+$app->post('/alunni', "AlunniController:postAlunni");
+$app->put('/alunni/{id}', "AlunniController:putAlunno");
+$app->delete('/alunni/{id}', "AlunniController:deleteAlunno");
 $app->run();
